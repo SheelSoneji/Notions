a479fc9dca8d3707d508a9340d44d6ad0bcea9ce
Sheel <sheel.soneji@gmail.com>
2020-07-13 19:35:21 -0700

    made a test case for testing the creation of notions

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+from .models import Notion
 # Create your tests here.
+
+User = get_user_model()
+
+
+class NotionTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="cfe", password="somepassword")
+
+    def test_notion_created(self):
+        notion_obj = Notion.objects.create(content="MyNotion", user=self.user)
+        self.assertEqual(notion_obj.id, 1)
+        self.assertEqual(notion_obj.user, self.user)
